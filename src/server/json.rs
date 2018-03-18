9cf4cf16f31ec7d3860ee33031cb891ca2fe4e96
Jeremy Soller <jackpot51@gmail.com>
2018-03-17 19:24:46 -0600

    Improve on charts

--- src/server/json.rs
+++ src/server/json.rs
@@ -16,27 +16,27 @@ pub struct JsonDump {
 #[get("/json")]
 pub fn json(connection_mutex: State<ConnectionMutex>) -> Result<Json<JsonDump>, String> {
     let connection = connection_mutex.lock().map_err(|err| format!("{}", err))?;
-    
+
     let wallets = wallets::table
         .order(wallets::id.asc())
         .load::<Wallet>(&*connection)
         .map_err(|err| format!("{}", err))?;
-    
+
     let accounts = accounts::table
         .order(accounts::id.asc())
         .load::<Account>(&*connection)
         .map_err(|err| format!("{}", err))?;
-        
+
     let positions = positions::table
         .order(positions::id.asc())
         .load::<Position>(&*connection)
         .map_err(|err| format!("{}", err))?;
-    
+
     let transactions = transactions::table
-        .order((transactions::time.desc(), transactions::id.desc()))
+        .order((transactions::time.asc(), transactions::id.asc()))
         .load::<Transaction>(&*connection)
         .map_err(|err| format!("{}", err))?;
-    
+
     Ok(Json(JsonDump {
         wallets,
         accounts,
