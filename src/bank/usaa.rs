db237499f10c43f5b740b92eb9f96b0c9b72aa17
Jeremy Soller <jeremy@system76.com>
2021-02-08 09:42:56 -0700

    Update USAA settings

--- src/bank/usaa.rs
+++ src/bank/usaa.rs
@@ -2,15 +2,31 @@ use bank_ofx;
 
 bank_ofx!("usaa", Usaa, {
     fn url(&self) -> &str {
-        "https://service2.usaa.com/ofx/OFXServlet"
+        "https://df3cx-services.1fsapi.com/casm/usaa/access.ofx"
+    }
+
+    fn ofx_ver(&self) -> &str {
+        "103"
+    }
+
+    fn pretty(&self) -> bool {
+        true
     }
 
     fn fid(&self) -> &str {
-        "24591"
+        "67811"
     }
 
     fn fid_org(&self) -> &str {
-        "USAA"
+        "USAA Federal Savings Bank"
+    }
+
+    fn app_id(&self) -> &str {
+        "QMOFX"
+    }
+
+    fn app_ver(&self) -> &str {
+        "2300"
     }
 
     fn broker_id(&self) -> &str {
